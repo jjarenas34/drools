c59f61cff60893ce9d520a3fcffb74a98e46a174
JUAN JESÚS ARENAS IPARRAGUIRRE <jjarenas@pucp.edu.pe>
2021-12-01 08:45:46 -0500

    principal

---
+++ src/main/java/com/example/rulesengine/RulesengineApplication.java
@@ -0,0 +1,13 @@
+package com.example.rulesengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RulesengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RulesengineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rulesengine/RulesengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rulesengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RulesengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
